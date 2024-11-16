adb16efa947f7b96600a81d30d6034b190c421f5
bulleux <0786623352@orange.fr>
2024-11-16 10:42:59 +0100

    first commit

---
+++ script_docker.sh
@@ -0,0 +1,12 @@
+# like root user
+apt update
+apt upgrade -y
+apt install -y apt-transport-https ca-certificates curl gnupg lsb-release
+curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/debian $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+apt update
+apt install -y docker-ce docker-ce-cli containerd.io
+docker --version
+usermod -aG docker $USER
+newgrp docker
+systemctl restart ssh
